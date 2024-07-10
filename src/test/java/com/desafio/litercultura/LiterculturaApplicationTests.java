eee6740b62e50b1a192984596d9378dafb0e3b68
wataredev <158059889+wataredev@users.noreply.github.com>
2024-07-10 11:56:56 -0300

    feat: projeto-base

---
+++ src/test/java/com/desafio/litercultura/LiterculturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.litercultura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterculturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
